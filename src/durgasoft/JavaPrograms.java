56c96e57ffd0dd6f26ec73ab3ba40f7b23805485
lenovo <lenovo@DESKTOP-B38FRTG>
2022-02-12 20:01:58 +0530

    added concate two string programs

--- src/durgasoft/JavaPrograms.java
+++ src/durgasoft/JavaPrograms.java
@@ -726,6 +726,21 @@ public class JavaPrograms {
 		
 	}
 	
+	@Test
+	public static void P29_ConcatStrings() {
+	    // create first string
+	    String first = "Java ";
+	    System.out.println("First String: " + first);
+
+	    // create second
+	    String second = "Programming";
+	    System.out.println("Second String: " + second);
+
+	    // join two strings
+	    String joinedString = first.concat(second);
+	    System.out.println("Joined String: " + joinedString);
+	  }
+	
 }
 
 
